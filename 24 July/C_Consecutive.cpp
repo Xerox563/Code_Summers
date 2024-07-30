47c3ab41e724a63d27d12d861e7bc26a5d06f212
Amit Gangwar <gangwaramit449@gmail.com>
2024-07-30 12:29:23 +0530

    Added All Stack Conversion

--- 24 July/C_Consecutive.cpp
+++ 24 July/C_Consecutive.cpp
@@ -1,21 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+
     long long N, Q;
     cin >> N >> Q;
-    string str;
-    cin >> str;
-    /* Solution Goes here */
-    // precompute
-    int cnt = 0;
-    vector<int> prefix(N);
-    int pre = str[0];
-    for(int i=1;i<=N;i++) {
-        if(str[i] == pre) {
-            cnt++;
+    string S;
+    cin >> S;
+
+    // Precompute the prefix sums of consecutive same characters
+    vector<int> prefix(N, 0);
+    for (int i = 1; i < N; ++i)
+    {
+        prefix[i] = prefix[i - 1];
+        if (S[i] == S[i - 1])
+        {
+            prefix[i]++;
         }
-        pre
     }
+
+    // Answer each query
+    while (Q--)
+    {
+        int l, r;
+        cin >> l >> r;
+        l--;
+        r--;
+
+        cout << prefix[r] - prefix[l] << endl;
+    }
+
     return 0;
 }
